f29a0b559305a8400e3bf9619380034d4ebe2ecb
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-22 03:10:56 +0800

    refactor: replace FaceIcon with cleaner outline version

--- components/icons/FaceIcon.tsx
+++ components/icons/FaceIcon.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function FaceIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-			{...props}><title>PrimeFaces</title><path d="M6.43 0 3.875 2.564l3.833.367L8.988 0Zm3.104 0L8.081 3.465 1.5 2.931l1.094 4.58 7.853 2.931h1.095V0Zm2.74 0v10.442h1.278l7.853-2.93 1.095-4.58-6.756.547L14.283 0Zm2.556 0 1.28 2.931 3.834-.367L17.387 0ZM2.778 8.244v6.229l4.383 3.663v-5.68l1.278-1.83-1.643.366-2.009-2.016zm18.261 0-2.009.732-2.009 2.016-1.643-.367 1.278 1.832v5.68l4.383-3.664zM9.17 10.625l-1.462 2.199v8.243l1.097 1.65L10.083 24h3.652l1.278-1.284 1.096-1.649v-8.243l-1.46-2.199-.914.55h-3.652zm-4.383 6.23v2.38l2.374 2.382V18.87zm14.243 0-2.374 2.015v2.747l2.374-2.381z"/></svg>
-  )
+	return (
+		<svg
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="currentColor"
+			{...props}
+		>
+			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+			<g
+				id="SVGRepo_tracerCarrier"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			></g>
+			<g id="SVGRepo_iconCarrier">
+				<path
+					stroke="#000000"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+					d="M7 3H5a2 2 0 0 0-2 2v2m0 10v2a2 2 0 0 0 2 2h2m10 0h2a2 2 0 0 0 2-2v-2m0-10V5a2 2 0 0 0-2-2h-2"
+				></path>
+				<circle
+					cx="12"
+					cy="9"
+					r="3"
+					stroke="#000000"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+				></circle>
+				<path
+					stroke="#000000"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+					d="M17 16c0-2.21-2.239-4-5-4s-5 1.79-5 4"
+				></path>
+			</g>
+		</svg>
+	);
 }
