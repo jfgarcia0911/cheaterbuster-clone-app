1489bbc294f1ab22bef6df23274f91ed69764127
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-22 03:12:13 +0800

    refactor: replace SearchIcon with cleaner outline version

--- components/icons/SearchIcon.tsx
+++ components/icons/SearchIcon.tsx
@@ -1,16 +1,62 @@
 import React from "react";
 
 export default function SearchIcon(props: React.SVGProps<SVGSVGElement>) {
-	return (
-		<svg
-			role="img"
-			viewBox="0 0 24 24"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="currentColor"
-			{...props}
-		>
-			<title>SearXNG</title>
-			<path d="m13.716 17.261 6.873 6.582L24 20.282l-6.824-6.536a9.11 9.11 0 0 0 1.143-4.43c0-5.055-4.105-9.159-9.16-9.159S0 4.261 0 9.316c0 5.055 4.104 9.159 9.159 9.159a9.11 9.11 0 0 0 4.557-1.214ZM9.159 2.773a6.546 6.546 0 0 1 6.543 6.543 6.545 6.545 0 0 1-6.543 6.542 6.545 6.545 0 0 1-6.542-6.542 6.545 6.545 0 0 1 6.542-6.543ZM7.26 5.713a4.065 4.065 0 0 1 4.744.747 4.064 4.064 0 0 1 .707 4.749l1.157.611a5.376 5.376 0 0 0-.935-6.282 5.377 5.377 0 0 0-6.274-.987l.601 1.162Z" />
-		</svg>
-	);
+  return (
+    <svg
+      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      transform="rotate(45)"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <defs>
+          <style>
+            {`
+              .cls-1 { fill: #898c90; }
+              .cls-2 { fill: #a9adb1; }
+              .cls-3 { fill: #4c5665; }
+              .cls-4 { fill: #83c0ec; }
+              .cls-5 { fill: #3398d6; }
+            `}
+          </style>
+        </defs>
+        <g id="Search">
+          <rect
+            className="cls-1"
+            height="4"
+            transform="translate(47 60) rotate(180)"
+            width="1"
+            x="23"
+            y="28"
+          />
+          <rect
+            className="cls-2"
+            height="4"
+            transform="translate(50 60) rotate(180)"
+            width="2"
+            x="24"
+            y="28"
+          />
+          <path
+            className="cls-3"
+            d="M24.5,32h0A2.5,2.5,0,0,1,27,34.5V47a1,1,0,0,1-1,1H23a1,1,0,0,1-1-1V34.5A2.5,2.5,0,0,1,24.5,32Z"
+            transform="translate(49 80) rotate(180)"
+          />
+          <circle className="cls-1" cx="25" cy="15" r="15" />
+          <circle className="cls-4" cx="25" cy="15" r="13" />
+          <path
+            className="cls-5"
+            d="M15.5,16.5a1,1,0,0,1-1-1A10.512,10.512,0,0,1,25,5a1,1,0,0,1,0,2,8.51,8.51,0,0,0-8.5,8.5A1,1,0,0,1,15.5,16.5Z"
+          />
+        </g>
+      </g>
+    </svg>
+  );
 }
