f83306d04ba9c261d0335934c9b072f4759035b4
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-25 00:25:30 +0800

    feat: add updated SVG icons

--- components/icons/FaceIcon.tsx
+++ components/icons/FaceIcon.tsx
@@ -3,42 +3,25 @@ import React from "react";
 export default function FaceIcon(props: React.SVGProps<SVGSVGElement>) {
 	return (
 		<svg
-			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-scan-face-icon lucide-scan-face"
 			fill="currentColor"
 			{...props}
 		>
-			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-			<g
-				id="SVGRepo_tracerCarrier"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			></g>
-			<g id="SVGRepo_iconCarrier">
-				<path
-					stroke="#000000"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					strokeWidth="2"
-					d="M7 3H5a2 2 0 0 0-2 2v2m0 10v2a2 2 0 0 0 2 2h2m10 0h2a2 2 0 0 0 2-2v-2m0-10V5a2 2 0 0 0-2-2h-2"
-				></path>
-				<circle
-					cx="12"
-					cy="9"
-					r="3"
-					stroke="#000000"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					strokeWidth="2"
-				></circle>
-				<path
-					stroke="#000000"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					strokeWidth="2"
-					d="M17 16c0-2.21-2.239-4-5-4s-5 1.79-5 4"
-				></path>
-			</g>
+			<path d="M3 7V5a2 2 0 0 1 2-2h2" />
+			<path d="M17 3h2a2 2 0 0 1 2 2v2" />
+			<path d="M21 17v2a2 2 0 0 1-2 2h-2" />
+			<path d="M7 21H5a2 2 0 0 1-2-2v-2" />
+			<path d="M8 14s1.5 2 4 2 4-2 4-2" />
+			<path d="M9 9h.01" />
+			<path d="M15 9h.01" />
 		</svg>
 	);
 }
---
+++ components/icons/UserIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function UserIcon(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="34"
+			height="34"
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			{...props}
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-user-icon lucide-user"
+		>
+			<path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+			<circle cx="12" cy="7" r="4" />
+		</svg>
+	);
+}
